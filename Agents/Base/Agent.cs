615db735cc97a87d7b8e94123e5bd7683d518653
abelpenton <2235penton@gmail.com>
2018-05-07 22:31:53 -0700

    Afd Validationfor actions in Agenst

--- Agents/Base/Agent.cs
+++ Agents/Base/Agent.cs
@@ -10,14 +10,34 @@ namespace Agents.Base
     {
         public ActionPlay lastAction { get; set; }
         private Word word { get; set; }
-
+        private (int r,int c)[] addr { get; set; }
+        private int[] columns { get; set; }
         public Agent(int x, int y) : base(x, y)
         {
+            addr = new (int,int)[4] { (1,0),(-1,0),(0,1),(0,-1)};
         }
 
-        private bool canMoveBoll()
+        private bool NotGoals(int x, int y)
         {
-            throw new Exception();
+            return !word._goals.Exists(g => g._x == x && g._y == y);
+        }
+        private bool NotMember(int x, int y)
+        {
+            return !(word.team1._members.Exists(m => m._x == x && m._y == y) && 
+                   word.team2._members.Exists(m => m._x == x && m._y == y));
+        }
+
+        private bool NotBall(int x, int y)
+        {
+            return !word.balls.Exists(b => b._x == x && b._y == y);
+        }
+        private bool ValidPos(int x, int y)
+        {
+            return x >= 0 && x < word._n && y >= 0 && y < word._n && NotGoals(x, y) && NotMember(x, y) && NotBall(x, y);
+        }
+        private bool canMoveBoll(int nextX, int nextY, (int r,int c) addr)
+        {
+            return word.balls.Exists(b => b._x == nextX && b._y == nextY) && ValidPos(nextX + addr.r, nextY + addr.c);
         }
         public void moveBoll()
         {
@@ -38,19 +58,23 @@ namespace Agents.Base
         public void doAction(Word word)
         {
             this.word = word;
-            if (canMoveBoll())
+            for (int i = 0; i < 4; i++)
             {
-               moveBoll();
-            }
-            else
-            {
-                if (canMove())
-                    move();
+                if (canMoveBoll(this._x + addr[i].r,this._y+addr[i].c, addr[i]))
+                {
+                    moveBoll();
+                }
                 else
                 {
-                    lastAction = ActionPlay.Pass;
+                    if (canMove())
+                        move();
+                    else
+                    {
+                        lastAction = ActionPlay.Pass;
+                    }
                 }
             }
+            
         }
     }
 }
