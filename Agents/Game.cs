6653ba9d9693f9cea492d1a8ac88e323b0f8573f
abelpenton <2235penton@gmail.com>
2018-05-07 21:49:05 -0700

    Add functionality in Game

--- Agents/Game.cs
+++ Agents/Game.cs
@@ -1,4 +1,5 @@
-﻿using Agents.Map;
+﻿using Agents.Base.Enum;
+using Agents.Map;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -17,18 +18,46 @@ namespace Agents
 
         public void StartSimulation(int turn)
         {
+            var r = new Random();
             while(turn-- > 0)
             {
                 if (EndOfGame())
                     return;
 
-                //TODO
+                var lik = r.Next(0, 2);
+                if (lik == 0)
+                {
+                    Actions(word.team1, word.team2);
+                }
+                else
+                    Actions(word.team2, word.team1);
+
             }
         }
 
+        private void Actions(Team t1, Team t2)
+        {
+            foreach (var a in t1._members)
+            {
+                a.doAction();
+            }
+            foreach (var a in t2._members)
+            {
+                a.doAction();
+            }
+        }
         private bool EndOfGame()
         {
-            return true;
+            var result = true;
+            foreach (var a in word.team1._members)
+            {
+                result = result && a.lastAction == ActionPlay.Pass;
+            }
+            foreach (var a in word.team2._members)
+            {
+                result = result && a.lastAction == ActionPlay.Pass;
+            }
+            return result;
         }
 
       
